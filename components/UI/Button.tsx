51336591b6b3fb0ecf54d89fd4da8c03e7a4def2
Deric F <dericgf@gmail.com>
2021-03-29 14:34:39 -0600

    Add abstracted Button component

---
+++ components/UI/Button.tsx
@@ -0,0 +1,77 @@
+import React, {
+  FunctionComponent,
+  PropsWithChildren,
+  ReactChildren,
+} from "react";
+
+type ButtonColor = "white" | "blue" | "green" | "red";
+
+type PropType = JSX.IntrinsicAttributes & React.ButtonHTMLAttributes<HTMLButtonElement>
+interface Props extends PropType {
+  color?: ButtonColor;
+  loading?: boolean;
+  className?: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  icon?: JSX.Element;
+}
+
+export const Button: FunctionComponent<Props> = ({
+  color = "white",
+  loading = false,
+  className = "",
+  // onClick = () => {},
+  children,
+  type = "button",
+  icon,
+  ...props
+}) => {
+  let colorClasses = "";
+  const {key} = props
+  console.log("Props..", props, key);
+  switch (color) {
+    case "blue":
+      colorClasses = `text-white bg-blue-500 focus:ring-blue-400 hover:bg-blue-700`;
+      break;
+    case "white":
+      colorClasses = `text-gray-900 bg-white focus:ring-gray-100 hover:bg-gray-800 hover:text-white`;
+      break;
+    case "green":
+      colorClasses = `text-white bg-green-500 focus:ring-green-400 hover:bg-green-700`;
+      break;
+    case "red":
+      colorClasses = `text-white bg-red-500 focus:ring-red-400 hover:bg-red-700`;
+      break;
+    default:
+      break;
+  }
+  return (
+    <button
+      className={`flex justify-center align-middle py-2 px-4 mx-2 my-2 ${colorClasses} font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-opacity-75 ${className}`}
+      type={type}
+      // onClick={onClick}
+      {...props}
+      
+    >
+      {/* {icon && icon} */}
+      {loading ? (
+        <svg
+          className="h-5 w-5 mr-3 animate-spin"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+          />
+        </svg>
+      ) : (
+        children
+      )}
+    </button>
+  );
+};
