78d038a55662c5b104c0d75716fa0652c67f706a
Deric F <dericgf@gmail.com>
2021-03-29 14:34:55 -0600

    Add Login Button

---
+++ components/UI/LoginButton.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+import { useAuth } from "../../hooks/useAuth";
+import { Button } from "./Button";
+
+export const LoginButton = () => {
+  const router = useRouter();
+
+  return (
+    <Button color="white" onClick={() => router.push("/login")}>
+      login
+    </Button>
+  );
+};
