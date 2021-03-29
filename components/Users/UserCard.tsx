5037fd6f82910e49d79df3fad16b1546c523601e
Deric F <dericgf@gmail.com>
2021-03-29 14:46:06 -0600

    Add basic user card

---
+++ components/Users/UserCard.tsx
@@ -0,0 +1,37 @@
+import { useAuth, User } from "../../hooks/useAuth";
+
+
+export const UserProfileCard = () => {
+  const { user } = useAuth();
+  return (
+    <div className="max-w-md mx-auto my-6 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div className="md:flex">
+        <div className="md:flex-shrink-0">
+          {/* <img
+            className="h-48 w-full object-cover md:w-48"
+            src="/img/store.jpg"
+            alt="Man looking at item at a store"
+          /> */}
+
+			<svg className="w-full h-48 md:w-48"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+				<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
+			</svg>
+        </div>
+        <div className="p-8">
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+            {user.username}
+          </div>
+          <a
+            href="#"
+            className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+          >
+            {user.email}
+          </a>
+          <p className="mt-2 text-gray-500">
+            {user.bio}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
