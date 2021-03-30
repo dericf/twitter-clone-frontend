1ddeb09176aeb46d517064a26e57aa130c418ce5
Deric F <dericgf@gmail.com>
2021-03-30 15:06:44 -0600

    Add remaining Tweet Crud functions

--- crud/tweets.ts
+++ crud/tweets.ts
@@ -1,4 +1,4 @@
-import { TweetResponse } from "../schema/Tweet";
+import { TweetCreateRequestBody, TweetResponse, TweetUpdateRequestBody } from "../schema/Tweet";
 
 export const getAllTweets = async (userId = null): Promise<TweetResponse> => {
   try {
@@ -26,6 +26,90 @@ export const getAllTweets = async (userId = null): Promise<TweetResponse> => {
     }
   } catch (error) {
     console.log("Caught error: ", error);
+    return null;
+  }
+};
+
+const createNewTweet = async (
+  requestBody: TweetCreateRequestBody,
+): Promise<EmptyResponse> => {
+  try {
+    const res = await fetch(`http://localhost:8001/tweets/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+      body: JSON.stringify(requestBody),
+      credentials: "include",
+    });
+    if (res.status >= 200 && res.status < 300) {
+      const json: EmptyResponse = await res.json();
+      return json;
+    } else {
+      // Non-200 response
+      return null;
+    }
+  } catch (error) {
+    // Actual Error
+    console.log("Caught error :>> ", error);
+    return null;
+  }
+};
+
+
+
+const updateTweet = async (
+  requestBody: TweetUpdateRequestBody,
+  tweetId: number,
+): Promise<EmptyResponse> => {
+  try {
+    const res = await fetch(`http://localhost:8001/tweets/${tweetId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+      body: JSON.stringify(requestBody),
+      credentials: "include",
+    });
+    if (res.status >= 200 && res.status < 300) {
+      const json: EmptyResponse = await res.json();
+      return json;
+    } else {
+      // Non-200 response
+      return null;
+    }
+  } catch (error) {
+    // Actual Error
+    console.log("Caught error :>> ", error);
+    return null;
+  }
+};
+
+
+const deleteTweet = async (
+  tweetId: number,
+): Promise<EmptyResponse> => {
+  try {
+    const res = await fetch(`http://localhost:8001/tweets/${tweetId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+      credentials: "include",
+    });
+    if (res.status >= 200 && res.status < 300) {
+      const json: EmptyResponse = await res.json();
+      return json;
+    } else {
+      // Non-200 response
+      return null;
+    }
+  } catch (error) {
+    // Actual Error
+    console.log("Caught error :>> ", error);
     return null;
   }
 };
