1f292778f0608a2e9b6f5c986a2a66394dff5999
Deric F <dericgf@gmail.com>
2021-03-31 01:08:45 -0600

    Add proper left sidebar

---
+++ components/UI/Nav/LeftSidebarNav.tsx
@@ -0,0 +1,149 @@
+import { Button } from "../Button";
+import { FunctionComponent } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+interface PropType extends JSX.IntrinsicAttributes {}
+
+export const LeftSidebar: FunctionComponent<PropType> = (props) => {
+  const router = useRouter;
+
+  return (
+    <>
+      {/* Left Sidebar */}
+      <Link href="/" as="/">
+        <Button
+          color="blue"
+          className="text-xs sm:text-sm md:text-lg flex justify-between  flex-none justify-self-start align-middle"
+        >
+          Discover
+          <svg
+            className="h-6 w-6 shadow-lg"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </Button>
+      </Link>
+      <Link href="/tweets" as="/tweets">
+        <Button
+          color="blue"
+          className="text-xs sm:text-sm md:text-lg flex justify-between  flex-none justify-self-start align-middle"
+        >
+          Tweets
+          <svg
+            className="h-6 w-6 shadow-lg"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+            />
+          </svg>
+        </Button>
+      </Link>
+
+      <Link href="/likes" as="/likes">
+        <Button
+          color="blue"
+          className="text-xs sm:text-sm md:text-lg flex justify-between  flex-none justify-self-start align-middle"
+        >
+          Likes
+          <svg
+            className="h-6 w-6 shadow-lg"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          </svg>
+        </Button>
+      </Link>
+      <Link href="/followers" as="/followers">
+        <Button
+          color="blue"
+          className="text-xs sm:text-sm md:text-lg flex justify-between  flex-none justify-self-start align-middle"
+        >
+          Followers
+          <svg
+            className="h-6 w-6 shadow-lg"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+            />
+          </svg>
+        </Button>
+      </Link>
+      <Link href="/comments" as="/comments">
+        <Button
+          color="blue"
+          className="text-xs sm:text-sm md:text-lg flex justify-between  flex-none justify-self-start align-middle"
+        >
+          Comments
+          <svg
+            className="h-6 w-6 shadow-lg"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          </svg>
+        </Button>
+      </Link>
+      <Link href="/profile" as="/profile">
+        <Button
+          color="blue"
+          className="text-xs sm:text-sm md:text-lg flex justify-between  flex-none justify-self-start align-middle"
+        >
+          Profile
+          <svg
+            className="h-6 w-6 shadow-lg"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </Button>
+      </Link>
+    </>
+  );
+};
