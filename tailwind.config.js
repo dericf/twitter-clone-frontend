05acff1e3f5fb5c6904b93602329e00132604648
Deric F <dericgf@gmail.com>
2021-03-31 21:12:00 -0600

    Add all tailwind colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,44 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [
-    './pages/**/*.{js,jsx,ts,tsx,vue}',
-    './components/**/*.{js,jsx,ts,tsx,vue}',
+    "./pages/**/*.{js,jsx,ts,tsx,vue}",
+    "./components/**/*.{js,jsx,ts,tsx,vue}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      rose: colors.rose,
+      pink: colors.pink,
+      fuchsia: colors.fuchsia,
+      purple: colors.purple,
+      violet: colors.violet,
+      indigo: colors.indigo,
+      blue: colors.blue,
+      lightBlue: colors.lightBlue,
+      cyan: colors.cyan,
+      teal: colors.teal,
+      emerald: colors.emerald,
+      green: colors.green,
+      lime: colors.lime,
+      yellow: colors.yellow,
+      amber: colors.amber,
+      orange: colors.orange,
+      red: colors.red,
+      warmGray: colors.warmGray,
+      trueGray: colors.trueGray,
+      gray: colors.gray,
+      coolGray: colors.coolGray,
+      blueGray: colors.blueGray,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
