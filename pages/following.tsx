0181fd9492f39efa500f2294fd9f0d7a85bebadb
Deric F <dericgf@gmail.com>
2021-04-04 16:37:11 -0600

    Implement following page
    shows all users that the user follows and their 3 most recent tweets

---
+++ pages/following.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { Button } from "../components/UI/Button";
+import { Layout } from "../components/UI/Layout";
+import { getAllTweets } from "../crud/tweets";
+import { useAlert } from "../hooks/useAlert";
+import { Tweet, TweetResponse } from "../schema/Tweet";
+import { TweetCard } from "../components/Tweets/TweetCard";
+import { useAuth } from "../hooks/useAuth";
+import { Protected } from "../components/Auth/Protected";
+import { useStore } from "../hooks/useStore";
+import { getAllFollows } from "../crud/follows";
+import { UserProfileCard } from "../components/Users/UserCard";
+import { FollowCard } from "../components/Follows/FollowsCard";
+
+export default function Home() {
+  const { sendAlert, sendError } = useAlert();
+  const { user } = useAuth();
+  const { follows, setFollows } = useStore();
+
+  useEffect(() => {
+    console.log("Running effect...");
+    if (!user) return;
+    (async () => {
+      const allFollows = await getAllFollows(user.id);
+      setFollows(allFollows);
+    })();
+  }, [user]);
+
+  return (
+    <Layout pageTitle="People I Follow" isProtected={true}>
+      {follows &&
+        follows.map((follow) => (
+          <FollowCard follow={follow} key={follow.username} />
+        ))}
+    </Layout>
+  );
+}
