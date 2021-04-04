63cbf4a6ba74e5430a49622461022e2f84ae89ca
Deric F <dericgf@gmail.com>
2021-04-04 16:39:21 -0600

    Abstract LeftSidebarLinks into own component dynamically render list of sidebar links

--- components/UI/Nav/SidebarLink.tsx
+++ components/UI/Nav/SidebarLink.tsx
@@ -9,20 +9,33 @@ import Link from "next/link";
 type PropType = JSX.IntrinsicAttributes &
   React.ButtonHTMLAttributes<HTMLButtonElement> &
   React.AnchorHTMLAttributes<HTMLAnchorElement>;
-interface Props extends PropType {}
+interface Props extends PropType {
+  isActiveLink: boolean;
+}
 
-export const SidebarLink: FunctionComponent<PropType> = ({
+export const SidebarLink: FunctionComponent<Props> = ({
   href,
   onClick,
   children,
+  isActiveLink = null,
   ...props
 }) => {
-  let colorClasses = "";
 
   return (
     <Link href={href} as={href}>
       <button
-        className={`py-2 px-4 mx-2 my-2 font-semibold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-opacity-75 text-white bg-lightBlue-700 focus:ring-lightBlue-900 hover:bg-lightBlue-800 text-xs sm:text-sm md:text-lg flex justify-between  flex-none justify-self-start align-middle`}
+        className={`py-2 px-4 mx-2 my-2 font-semibold rounded-md shadow-md 
+        focus:outline-none 
+        focus:ring-2 
+        focus:ring-opacity-75 text-white 
+        focus:ring-lightBlue-900 
+        
+        
+        ${isActiveLink ? "bg-lightBlue-800 " : "bg-lightBlue-700 "}
+        hover:bg-lightBlue-800 
+
+        text-xs sm:text-sm md:text-lg 
+        flex justify-between flex-none justify-self-start align-middle`}
         type="button"
         onClick={onClick}
         {...props}
