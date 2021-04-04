d83514d24fb931aee39ded9e0c64f4cda1d14302
Deric F <dericgf@gmail.com>
2021-04-04 16:39:36 -0600

    Make main app title more responsive

--- components/UI/MainAppTitle.tsx
+++ components/UI/MainAppTitle.tsx
@@ -1,4 +1,4 @@
 
 export const MainTitle = () => {  
-  return <h1 className="text-center text-4xl">Twitter Clone</h1>;
+  return <h1 className="text-center text-2xl  sm:text-3xl md:text-4xl">Twitter Clone</h1>;
 };
