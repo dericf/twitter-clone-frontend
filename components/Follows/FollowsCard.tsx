d7989a3b82141e87a682003d881cdb0c438d9578
Deric F <dericgf@gmail.com>
2021-04-04 16:37:29 -0600

    Add a follows card

---
+++ components/Follows/FollowsCard.tsx
@@ -0,0 +1,68 @@
+import { useAuth } from "../../hooks/useAuth";
+import Link from "next/link";
+import { Follows } from "../../schema/Follows";
+import { useEffect, useState } from "react";
+import { Tweet } from "../../schema/Tweet";
+import { getAllTweets } from "../../crud/tweets";
+import { TweetCard } from "../Tweets/TweetCard";
+import { Button } from "../UI/Button";
+import { useRouter } from "next/router";
+
+interface Props {
+  follow: Follows;
+}
+
+export const FollowCard = (props: Props) => {
+  const { follow } = props;
+  const router = useRouter();
+  const { user } = useAuth();
+
+  const [userTweets, setUserTweets] = useState<Array<Tweet>>([]);
+
+  useEffect(() => {
+    if (!user) return;
+    (async () => {
+      setUserTweets(await getAllTweets(follow.userId, 3));
+    })().catch((err) => {
+      console.error(err);
+    });
+  }, [user]);
+
+  return (
+    <div className="max-w-md mx-auto my-6 bg-trueGray-100 rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div className="flex">
+        <div className="p-4 md:p-8 flex flex-col justify-center items-start flex-grow">
+          <Link href={`/user${follow.userId}/`} as={`/user/${follow.userId}`}>
+            <div className="uppercase tracking-wide cursor-pointer text-sm text-lightBlue-500 hover:text-lightBlue-700 font-semibold">
+              {follow.username}
+            </div>
+          </Link>
+
+          {/* <div className="block mt-1 text-lg leading-tight font-medium text-black">
+            {user.email}
+          </div> */}
+
+          <p className="mt-1 text-gray-500">{follow.bio}</p>
+
+          <div className="tracking-wide mt-1 text-sm text-lightBlue-500 font-semibold">
+            Latest Tweets by {follow.username}:
+          </div>
+          <div className="self-center align-self-center">
+            {userTweets &&
+              userTweets.map((tweet) => (
+                <TweetCard key={tweet.tweetId.toString()} tweet={tweet} />
+              ))}
+          </div>
+
+          <Button
+            color="blue"
+            fluid
+            onClick={() => router.push(`/user/${follow.userId}`)}
+          >
+            See all tweets by {follow.username}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
