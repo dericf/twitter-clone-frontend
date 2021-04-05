3c1b92915bcd29da2cfd6dfc563c34d20e9a98c0
Deric F <dericgf@gmail.com>
2021-04-04 22:26:43 -0600

    Add a check when sending error toasts. Now doesn't send duplicates

--- hooks/useAlert.tsx
+++ hooks/useAlert.tsx
@@ -1,14 +1,22 @@
 import { useToasts } from "react-toast-notifications";
 
 export const useAlert = () => {
-  const { addToast } = useToasts();
+  const { addToast, toastStack } = useToasts();
 
   const sendAlert = (text) => {
     addToast(text, { appearance: "success", autoDismiss: true });
   };
 
   const sendError = (text) => {
-    addToast(text, { appearance: "error", autoDismiss: true });
+    let duplicate = false;
+    toastStack.forEach((toast) => {
+      if (toast.content === text) {
+        duplicate = true;
+        return;
+      }
+    });
+    duplicate === false &&
+      addToast(text, { appearance: "error", autoDismiss: true });
   };
 
   const sendInfo = (text) => {
