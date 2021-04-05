5872870e871d47d764d5bf1c003271606894ca87
Deric F <dericgf@gmail.com>
2021-04-04 22:24:42 -0600

    Make user update request body properties optional

--- schema/User.ts
+++ schema/User.ts
@@ -10,24 +10,26 @@ export type UserResponse = Array<User>;
 
 export interface UserUpdateRequestBody {
   password: string;
-  newUsername: string;
-  newBio: string;
+  newUsername?: string;
+  newBio?: string;
 }
 
 export type UserUpdateResponse = User;
-
 
 export interface UserCreateRequestBody {
   username: string;
   email: string;
   bio: string;
   birthdate: string;
-	password: string;
+  password: string;
+}
+
+export interface UserRegisterForm extends UserCreateRequestBody {
+  confirmPassword: string;
 }
 
 export type UserCreateResponse = User;
 
-
 export interface UserDeleteRequestBody {
-	password: string
+  password: string;
 }
