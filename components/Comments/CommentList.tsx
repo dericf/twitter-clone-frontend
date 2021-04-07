5fe1aa5f11ce3b8273c292ef9d4ee5fd3e110a6a
Deric F <dericgf@gmail.com>
2021-04-07 14:09:28 -0600

    Add heading for comments list

--- components/Comments/CommentList.tsx
+++ components/Comments/CommentList.tsx
@@ -58,15 +58,19 @@ export const CommentList: FunctionComponent<PropType> = (props) => {
   }, []);
 
   return (
-    <div className="flex justify-center w-full mx-0 place-self-center">
-      <div className="flex flex-col w-full  text-white bg-blueGray-300 py-3 px-6 justify-center items-center">
+    <div className="flex justify-center w-full mx-0 mt-4 place-self-center">
+      <div className="flex flex-col w-full text-white bg-blueGray-300 py-0 px-0 justify-center items-center">
+        <h3 className="text-2xl mb-2 tracking-wide text-trueGray-800">
+          Comments
+        </h3>
+
         {comments.map((comment) => (
           // TODO: extract into CommentCard component
           <div
             key={comment.id}
-            className="flex bg-white text-blueGray-900 py-2 px-3 flex-col w-full mx-8 h-24 mb-4"
+            className="flex bg-white text-blueGray-900 py-0 px-0 flex-col w-full mx-8 mb-4"
           >
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center px-4 py-1">
               <Link
                 as={`/user/${comment.userId}`}
                 href={`/users/${comment.userId}`}
@@ -92,9 +96,28 @@ export const CommentList: FunctionComponent<PropType> = (props) => {
               )}
             </div>
 
-            <p className="text-md text-blueGray-800 font-normal">
+            <p className="text-md text-blueGray-800 font-normal px-4 py-2">
               {comment.content}
             </p>
+
+            <div className="bg-blueGray-400 mt-2 px-2">
+              <Button color="white" className="w-max">
+                <svg
+                  className="w-4 h-4 cursor-pointer"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+                  />
+                </svg>
+              </Button>
+            </div>
           </div>
         ))}
 
@@ -114,7 +137,7 @@ export const CommentList: FunctionComponent<PropType> = (props) => {
             rows={5}
             placeholder="Add your thoughts here"
             value={newComment}
-            className="px-4 py-2 bg-blueGray-600 text-white focus:ring-0 focus:border-blueGray-600"
+            className="px-4 py-4 bg-blueGray-600 text-white placeholder-trueGray-400 focus:ring-0 border-none focus:border-none"
             onChange={(e) => {
               if (e.currentTarget.value.length <= MAX_COMMENT_LENGTH) {
                 setNewComment(e.currentTarget.value);
@@ -128,8 +151,8 @@ export const CommentList: FunctionComponent<PropType> = (props) => {
           ></textarea>
           <Button
             type="submit"
-            color="green"
-            className=" flex items-center justify-center flex-grow mx-0"
+            color="white"
+            className="mx-0"
             disabled={newComment.length === 0}
           >
             <svg
