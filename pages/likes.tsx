d46f7e8e78d7727196e2f4ca078221b890498956
Deric F <dericgf@gmail.com>
2021-04-07 14:10:54 -0600

    Convert likes page to use new API Response and add pagination

--- pages/likes.tsx
+++ pages/likes.tsx
@@ -1,45 +1,80 @@
-import Head from "next/head";
 import { useEffect, useState } from "react";
 import { Button } from "../components/UI/Button";
 import { Layout } from "../components/UI/Layout";
-import { getAllLikedTweets, getAllTweets } from "../crud/tweets";
+import { getLikedTweets as getLikedTweets, getAllTweets } from "../crud/tweets";
 import { useAlert } from "../hooks/useAlert";
-import { TweetResponse } from "../schema/Tweet";
+import { Tweet } from "../schema/Tweet";
 import { TweetCard } from "../components/Tweets/TweetCard";
 import { useAuth } from "../hooks/useAuth";
-import { Protected } from "../components/Auth/Protected";
-import { useStore } from "../hooks/useStore";
-import { getAllTweetLikes } from "../crud/likes";
+import { DEFAULT_TWEET_LIMIT } from "../constants/constants";
 //
 export default function Home() {
-  const { sendAlert, sendError } = useAlert();
+  const { sendAlert, sendError, sendInfo } = useAlert();
   const { user } = useAuth();
 
-  const [likedTweets, setLikedTweets] = useState<TweetResponse>();
-  // const [tweets, setTweets] = useState<TweetResponse>([]);
-  // const { createTweet, refreshTweets, tweets } = useStore();
+  const [likedTweets, setLikedTweets] = useState<Array<Tweet>>([]);
+  const [loading, setLoading] = useState(false);
+  const [tweetPage, setTweetPage] = useState(1); // Pagination
 
   useEffect(() => {
     (async () => {
       // Get all Tweets and comments that a user has liked and
       // display them ordered newest first
-      const allTweetLikes = await getAllLikedTweets();
-      setLikedTweets(allTweetLikes);
+      try {
+        const { value: allTweetLikes, error } = await getLikedTweets();
+
+        if (error) throw new Error(error);
+        setLikedTweets(allTweetLikes);
+      } catch (error) {
+        sendError(error);
+      }
     })();
   }, [user]);
 
+  const loadMoreTweets = async () => {
+    setLoading(true);
+
+    // Get the next page of tweets
+    try {
+      const { value: nextPage, error } = await getLikedTweets(
+        tweetPage * DEFAULT_TWEET_LIMIT,
+      );
+      if (error) throw new Error(error);
+      if (nextPage.length === 0) {
+        // No more tweets
+        sendInfo("There are no more likes to show.");
+      } else {
+        // Increment page # for next time
+        setTweetPage(tweetPage + 1);
+        // Append new tweets to the end of the list
+        setLikedTweets([...likedTweets, ...nextPage]);
+      }
+    } catch (error) {
+      sendError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Layout isProtected={true} pageTitle="Tweets I Like">
-      {likedTweets &&
-        likedTweets.map((tweet) => (
-          <TweetCard key={tweet.tweetId.toString()} tweet={tweet} />
-        ))}
-      {likedTweets?.length === 0 && (
+      {likedTweets.length === 0 ? (
         <div className="text-xl text-white ">
           You have not liked any Tweets yet
         </div>
+      ) : (
+        <>
+          {likedTweets.map((tweet) => (
+            <TweetCard key={tweet.tweetId.toString()} tweet={tweet} />
+          ))}
+
+          <div className="h-40" onClick={loadMoreTweets}>
+            <Button className="mb-6" color="white" loading={loading}>
+              Load More Likes
+            </Button>
+          </div>
+        </>
       )}
-      {/* <div key={tweet.tweetId.toString()}>{JSON.stringify(tweet, null, 4)}</div> */}
     </Layout>
   );
 }
