a1f283d2ad6818a2cb082f599dafbac948c6947b
Deric F <dericgf@gmail.com>
2021-04-07 14:10:16 -0600

    Convert Follows to new API Wrapper structure

--- schema/Follows.ts
+++ schema/Follows.ts
@@ -1,3 +1,6 @@
+import { APIResponse } from "./API";
+import { EmptyResponse } from "./General";
+
 export interface Follows {
   userId: number;
   username: string;
@@ -10,12 +13,16 @@ export interface FollowsRequestBody {
   userId: number;
 }
 
-export type FollowsResponse = Array<Follows>;
+export type FollowsResponse = APIResponse<Array<Follows>>;
 
 export interface FollowsCreateRequestBody {
   followUserId: number;
 }
 
+export type FollowsCreateResponse = APIResponse<EmptyResponse>;
+
 export interface FollowsDeleteRequestBody {
   followUserId: number;
 }
+
+export type FollowsDeleteResponse = APIResponse<EmptyResponse>;
