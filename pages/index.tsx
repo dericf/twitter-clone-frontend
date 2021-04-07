f4ad5a073d49598cb665dfcb1d56a8b00e4dbf94
Deric F <dericgf@gmail.com>
2021-04-07 14:11:09 -0600

    Convert discover page to use new API Response and add pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,34 +10,83 @@ import { useStore } from "../hooks/useStore";
 import { getAllFollowers } from "../crud/followers";
 import { useAuth } from "../hooks/useAuth";
 import { getAllFollows } from "../crud/follows";
+import { DEFAULT_TWEET_LIMIT } from "../constants/constants";
 
 export default function Home() {
-  const { sendAlert, sendError } = useAlert();
-
-  // const [tweets, setTweets] = useState<TweetResponse>([]);
-
-  const { tweets, setTweets, setFollows } = useStore();
+  // Custom Hooks
+  const { sendAlert, sendError, sendInfo } = useAlert();
   const { user } = useAuth();
+  const { tweets, setTweets, setFollows } = useStore();
+
+  // Local state
+  const [loading, setLoading] = useState(true);
+  const [tweetPage, setTweetPage] = useState(1); // Pagination
 
   useEffect(() => {
     (async () => {
-      const allTweets = await getAllTweets();
-      setTweets(allTweets);
+      try {
+        const { value, error } = await getAllTweets();
 
-      user && setFollows(await getAllFollows(user.id));
+        if (error) throw new Error(error);
+        setTweets(value);
+      } catch (error) {
+        sendError(error);
+      }
+
+      if (user) {
+        try {
+          const { value: follows, error } = await getAllFollows(user.id);
+
+          if (error) throw new Error(error);
+          setFollows(follows);
+        } catch (error) {
+          sendError(error);
+        }
+      }
+      setLoading(false);
     })();
   }, []);
 
+  const loadMoreTweets = async () => {
+    setLoading(true);
+
+    // Get the next page of tweets
+    const { value: nextTweets, error } = await getAllTweets(
+      null,
+      tweetPage * DEFAULT_TWEET_LIMIT,
+    );
+    console.log("Next Page of Tweets: ", nextTweets);
+    if (nextTweets.length === 0) {
+      // No more tweets
+      sendInfo("There are no more tweets to show.");
+    } else {
+      // Increment page # for next time
+      setTweetPage(tweetPage + 1);
+      // Append new tweets to the end of the list
+      setTweets([...tweets, ...nextTweets]);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <Layout pageTitle="Discover New Tweets" silentAuth>
-      {tweets &&
-        tweets.map((tweet) => (
-          <TweetCard key={tweet.tweetId.toString()} tweet={tweet} />
-        ))}
-      {tweets?.length === 0 && (
+      {tweets.length === 0 ? (
         <div className="text-xl text-white ">
           No one has tweeted yet. Be the first one!
         </div>
+      ) : (
+        <>
+          {tweets &&
+            tweets.map((tweet) => (
+              <TweetCard key={tweet.tweetId.toString()} tweet={tweet} />
+            ))}
+          <div className="h-40" onClick={loadMoreTweets}>
+            <Button className="mb-6" color="white" loading={loading}>
+              Load More Tweets
+            </Button>
+          </div>
+        </>
       )}
     </Layout>
   );
