700e05eb3dd5903bf1a4adfb71988d0b32d62c29
Deric F <dericgf@gmail.com>
2021-04-07 14:15:27 -0600

    Add tab title to layout and add logic to set active sidebar link on page refresh

--- components/UI/Layout.tsx
+++ components/UI/Layout.tsx
@@ -1,5 +1,3 @@
-// import { useIsLoading } from "../../hookes/us";
-// import { LoginForm } from "./Auth/LoginForm";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
@@ -13,9 +11,10 @@ import Link from "next/link";
 import { MobileBottomNav } from "./Nav/MobileBottomNav";
 import { LeftSidebar } from "./Nav/LeftSidebarNav";
 import { NewTweetButton } from "../Tweets/NewTweetButton";
-import { LoadingSpinner } from "./LoadingSpinner";
 import { LoadingOverlay } from "./LoadingOverlay";
 import { useAlert } from "../../hooks/useAlert";
+import Head from "next/head";
+import { useStore } from "../../hooks/useStore";
 
 export const Layout = ({
   children,
@@ -23,14 +22,38 @@ export const Layout = ({
   noAuth = false,
   silentAuth = true,
   pageTitle = null,
+  tabTitle = pageTitle,
   onAuthSuccess = () => {},
+  loading = false,
 }) => {
   const { user, isAuthenticated, loadAuthState } = useAuth();
   const { sendError, sendAlert } = useAlert();
-  const [loading, setLoading] = useState(!user);
-
+  const [isLoading, setLoading] = useState(loading || !user);
+  const { activePage, setActivePage } = useStore();
   const router = useRouter();
+
   useEffect(() => {
+    console.log(router.asPath.startsWith("/"));
+    const path = router.asPath;
+
+    if (path === "/") {
+      setActivePage("discover");
+    } else if (path === "/tweets") {
+      setActivePage("tweets");
+    } else if (path === "/likes") {
+      setActivePage("likes");
+    } else if (path === "/profile") {
+      setActivePage("profile");
+    } else if (path === "/comments") {
+      setActivePage("comments");
+    } else if (path === "/following") {
+      setActivePage("following");
+    } else if (path === "/followers") {
+      setActivePage("followers");
+    } else {
+      setActivePage("");
+    }
+
     if (noAuth === true) {
       setLoading(false);
       return () => {};
@@ -58,54 +81,55 @@ export const Layout = ({
     }
   }, [router.isReady]);
 
-  // if (loading === true) {
-  //   return <LoadingOverlay />;
-  // }
-
   return (
-    <main className=" w-screen h-screen flex flex-col overflow-hidden">
-      <div className="flex flex-none px-2 py-2 w-full h-20 items-center justify-between shadow-xl border-lightBlue-800 border-b-2 bg-lightBlue-700 text-white">
-        <MainTitle />
-        {isAuthenticated ? (
-          <div className="flex">
-            <NewTweetButton />
-            <LogoutButton />
-          </div>
-        ) : router.asPath.includes("login") ? (
-          <></>
-        ) : (
-          <div className="flex">
-            <LoginButton />
-            <RegisterButton />
-          </div>
-        )}
-      </div>
-
-      <div className="flex-1 flex overflow-hidden ">
-        <nav className="hidden sm:flex flex-col w-24 sm:w-40 md:w-52 flex-shrink-0 justify-center px-0.5 sm:px-3 md:px-6 py-6 bg-white shadow-lg">
-          <LeftSidebar />
-        </nav>
-
-        {/* Scroll Wrapper */}
-        <div className="flex flex-1 pb-12 sm:pb-0 bg-blueGray-700">
-          {/* Main Content */}
-          <div className="flex-1 overflow-y-auto justify-between ">
-            {pageTitle && (
-              <h3 className="text-4xl text-center text-white my-4">
-                {pageTitle}
-              </h3>
-            )}
-            <div className="flex flex-col w-full h-full items-center justify-items-center">
-              {loading ? <LoadingOverlay /> : children}
+    <>
+      <Head>
+        <title>{tabTitle} | Twitter Clone</title>
+      </Head>
+      <main className=" w-screen h-screen flex flex-col overflow-hidden">
+        <div className="flex flex-none px-2 py-2 w-full h-20 items-center justify-between shadow-xl border-lightBlue-800 border-b-2 bg-lightBlue-700 text-white">
+          <MainTitle />
+          {isAuthenticated ? (
+            <div className="flex">
+              <NewTweetButton />
+              <LogoutButton />
             </div>
+          ) : router.asPath.includes("login") ? (
+            <></>
+          ) : (
+            <div className="flex">
+              <LoginButton />
+              <RegisterButton />
+            </div>
+          )}
+        </div>
 
-            <nav className="fixed sm:hidden flex justify-between items-center  bottom-0 w-screen h-12 flex-grow flex-shrink-0 -mx-4">
-              <MobileBottomNav />
-            </nav>
+        <div className="flex-1 flex overflow-hidden ">
+          <nav className="hidden sm:flex flex-col w-24 sm:w-40 md:w-52 flex-shrink-0 justify-center px-0.5 sm:px-3 md:px-6 py-6 bg-white shadow-lg">
+            <LeftSidebar />
+          </nav>
+
+          {/* Scroll Wrapper */}
+          <div className="flex flex-1 pb-12 sm:pb-0 bg-blueGray-700">
+            {/* Main Content */}
+            <div className="flex-1 overflow-y-auto justify-between ">
+              {pageTitle && (
+                <h3 className="text-4xl text-center text-white my-4">
+                  {pageTitle}
+                </h3>
+              )}
+              <div className="flex flex-col w-full h-full items-center justify-items-center">
+                {isLoading ? <LoadingOverlay /> : children}
+              </div>
+
+              <nav className="fixed sm:hidden flex justify-between items-center  bottom-0 w-screen h-12 flex-grow flex-shrink-0 -mx-4">
+                <MobileBottomNav />
+              </nav>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 // <div className="hidden md:flex md:flex-col flex-shrink-0 w-40 my-8 py-8">
