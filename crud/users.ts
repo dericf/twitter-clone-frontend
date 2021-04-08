88dd07005920b4235ecceb1e3ad6ecdfbf581d88
Deric F <dericgf@gmail.com>
2021-04-07 20:45:42 -0600

    Convert user crud functions to use new API wrapper

--- crud/users.ts
+++ crud/users.ts
@@ -1,39 +1,42 @@
+import {
+  APIResponse,
+  errorTextFromStatusCode,
+  responseDidSucceed,
+} from "../schema/API";
 import { UserNotAuthenticatedError } from "../schema/Errors";
 import { APIErrorResponse, EmptyResponse } from "../schema/General";
 import {
   User,
   UserDeleteRequestBody,
+  UserDeleteResponse,
   UserResponse,
   UserUpdateRequestBody,
   UserUpdateResponse,
 } from "../schema/User";
 
-export const getUserById = async (userId: number): Promise<User> => {
+export const getUserById = async (
+  userId: number,
+): Promise<APIResponse<User>> => {
   let url = new URL(`http://localhost:8001/users`);
   url.searchParams.set("userId", userId.toString());
-  try {
-    const res = await fetch(url.toString(), {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    });
-    // console.log("res.json :>> ", res.status);
-    if (res.status >= 200 && res.status < 300) {
-      const userResponse: UserResponse = await res.json();
-      if (userResponse.length === 0) return null;
-
-      // Return single user object
-      return userResponse[0];
-    } else {
-      const err: APIErrorResponse = await res.json();
-      throw new Error(`There was an issue. ${err.detail}`);
-    }
-  } catch (error) {
-    console.log("Caught error: ", error);
-    return null;
+  const res = await fetch(url.toString(), {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+  // console.log("res.json :>> ", res.status);
+  if (responseDidSucceed(res.status)) {
+    const users: Array<User> = await res.json();
+    return {
+      value: users.length > 0 ? users[0] : null,
+    };
+  } else {
+    return {
+      error: errorTextFromStatusCode(res.status),
+    };
   }
 };
 
@@ -41,54 +44,50 @@ export const updateUser = async (
   body: UserUpdateRequestBody,
 ): Promise<UserUpdateResponse> => {
   // Update the authenticated user's details (bio and/or username)
-  try {
-    const res = await fetch(`http://localhost:8001/users`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        accept: "application/json",
-      },
-      body: JSON.stringify(body),
-      credentials: "include",
-    });
-    if (res.status >= 200 && res.status < 300) {
-      return await res.json();
-    } else {
-      // Non-200 response
-      const err: APIErrorResponse = await res.json();
-      throw new Error(`Cannot update user. ${err.detail}`);
-    }
-  } catch (error) {
-    // Actual Error
-    console.log("Caught error :>> ", error);
-    throw error;
+  const res = await fetch(`http://localhost:8001/users`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+    body: JSON.stringify(body),
+    credentials: "include",
+  });
+  if (responseDidSucceed(res.status)) {
+    const json: User = await res.json();
+    return {
+      value: json,
+    };
+  } else {
+    return {
+      error: errorTextFromStatusCode(res.status),
+    };
   }
 };
 
-export const deleteUser = async (password: string): Promise<EmptyResponse> => {
+export const deleteUser = async (
+  password: string,
+): Promise<UserDeleteResponse> => {
   // User must be authenticated, but must also provide their password
   // for extra protection
-  try {
-    const res = await fetch(`http://localhost:8001/users/`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        accept: "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify({ password } as UserDeleteRequestBody),
-    });
-    if (res.status >= 200 && res.status < 300) {
-      const json: EmptyResponse = await res.json();
-      return json;
-    } else {
-      // Non-200 response
-      const err: APIErrorResponse = await res.json();
-      throw new Error(`Cannot delete user. ${err.detail}`);
-    }
-  } catch (error) {
-    // Actual Error
-    console.log("Caught error :>> ", error);
-    throw error;
+
+  const res = await fetch(`http://localhost:8001/users/`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ password } as UserDeleteRequestBody),
+  });
+  if (responseDidSucceed(res.status)) {
+    const json: EmptyResponse = await res.json();
+    return {
+      value: json,
+    };
+  } else {
+    return {
+      error: errorTextFromStatusCode(res.status),
+    };
   }
 };
