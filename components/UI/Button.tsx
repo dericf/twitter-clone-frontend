5646d3cefa3ade32f2b2aa1fa053c2fa738af382
Deric F <dericgf@gmail.com>
2021-04-09 21:32:32 -0600

    Add new prop to button to add or exclude margins
    Note: The button component is getting out of control and needs to be redesigned from the ground up.

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -19,6 +19,7 @@ interface Props extends PropType {
   fluid?: boolean;
   link?: string | null;
   animated?: boolean;
+  addMargins?: boolean;
 }
 
 export const Button: FunctionComponent<Props> = ({
@@ -33,6 +34,7 @@ export const Button: FunctionComponent<Props> = ({
   fluid = false,
   link = null,
   animated = false,
+  addMargins = true,
   ...props
 }) => {
   let colorClasses = "";
@@ -60,16 +62,14 @@ export const Button: FunctionComponent<Props> = ({
     <button
       disabled={disabled}
       className={`flex justify-center align-middle 
-      ${
-        disabled === true
-          ? "opacity-30 cursor-not-allowed !hover:bg-rose-800:"
-          : ""
-      }
+      ${disabled === true ? "opacity-30 cursor-not-allowed disabled" : ""}
       ${animated ? "transform hover:-translate-y-0.5" : ""}
-      text-sm py-2 px-4 mx-2 my-2 ${colorClasses} font-semibold rounded-sm ${
+      text-sm py-2 px-4 ${
+        addMargins ? "mx-2 my-2" : ""
+      }  ${colorClasses} font-semibold rounded-sm ${
         color === "transparent" ? "shadow-none" : "shadow-md"
       } focus:outline-none focus:ring-2 focus:ring-opacity-75 
-      ${fluid ? "flex-grow w-full" : ""}
+      ${fluid ? "flex-grow w-full" : ""} 
       ${className}`}
       type={type}
       // onClick={onClick}
@@ -79,20 +79,19 @@ export const Button: FunctionComponent<Props> = ({
       {loading ? (
         <>
           <svg
-            className="h-5 w-5 mr-3 animate-spin"
+            className="h-5 w-5 mx-4 animate-spin"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
             />
           </svg>
-          disabled
         </>
       ) : (
         children
