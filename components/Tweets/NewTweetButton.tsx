9c192ef9ed0589f45f56abbb3d6f06fb558cc88a
Deric F <dericgf@gmail.com>
2021-04-09 21:25:17 -0600

    Fix the logic for new tweet modal

--- components/Tweets/NewTweetButton.tsx
+++ components/Tweets/NewTweetButton.tsx
@@ -14,6 +14,9 @@ import { Button } from "../UI/Button";
 // import { createNewTweet } from "../../crud/tweets";
 import { useAlert } from "../../hooks/useAlert";
 import { useStore } from "../../hooks/useStore";
+import { MAX_TWEET_LENGTH } from "../../constants/constants";
+import { UserNotAuthenticatedError } from "../../schema/Errors";
+import { createNewTweet } from "../../crud/tweets";
 
 interface NewTweetPropType {
   showModal: boolean;
@@ -22,16 +25,22 @@ interface NewTweetPropType {
 
 const NewTweetModal: FunctionComponent<NewTweetPropType> = (props) => {
   const [content, setContent] = useState("");
-  const { createTweet } = useStore();
-
+  const { tweets, setTweets, createTweet } = useStore();
+  const router = useRouter();
   const { sendAlert, sendError } = useAlert();
 
   const inputRef = useRef<HTMLTextAreaElement>();
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    await createTweet({ content });
-    props.setShowModal(false);
+    try {
+      const { value, error } = await createNewTweet({ content });
+      if (error) throw new Error(error);
+      setTweets([{ ...value }, ...tweets]);
+      props.setShowModal(false);
+    } catch (error) {
+      sendError(error);
+    }
   };
 
   const closeOnEscape = (e) => {
@@ -60,7 +69,7 @@ const NewTweetModal: FunctionComponent<NewTweetPropType> = (props) => {
       <div className="modal-backdrop fixed bottom-0 left-0 top-0 right-0 px-4 backdrop-blur-md z-10">
         <div
           className="flex flex-col justify-center items-center
-            mx-auto p-8 rounded-lg 
+            mx-auto p-8 
             fixed 
             left-0 md:left-1/4 
             right-0 md:right-1/4
@@ -80,11 +89,15 @@ const NewTweetModal: FunctionComponent<NewTweetPropType> = (props) => {
             <textarea
               ref={inputRef}
               name="content"
-              className="bg-blueGray-600 tracking-wider flex-grow w-full max-lg  text-lg text-white p-4"
+              className="bg-blueGray-600 tracking-wider flex-grow w-full max-lg  text-lg text-white p-4 focus:ring-0 focus:border-white"
               id=""
               value={content}
               onChange={(e) => {
-                setContent(e.currentTarget.value);
+                e.currentTarget.value.length > MAX_TWEET_LENGTH
+                  ? sendError(
+                      `Tweet cannot exceed ${MAX_TWEET_LENGTH} in length.`,
+                    )
+                  : setContent(e.currentTarget.value);
               }}
               rows={5}
             ></textarea>
@@ -102,7 +115,9 @@ const NewTweetModal: FunctionComponent<NewTweetPropType> = (props) => {
                 color="blue"
                 type="submit"
                 className="flex-grow"
-                disabled={content.length === 0 || content.length > 80}
+                disabled={
+                  content.length === 0 || content.length > MAX_TWEET_LENGTH
+                }
               >
                 Confirm
               </Button>
@@ -123,8 +138,27 @@ export const NewTweetButton: FunctionComponent<PropType> = (props) => {
 
   return (
     <>
-      <Button color="green" onClick={() => setShowModal(true)}>
-        New Tweet
+      <Button
+        animated
+        color="green"
+        onClick={() => setShowModal(true)}
+        className="flex items-center justify-between"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4 mr-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+        Tweet
       </Button>
       {showModal && (
         <NewTweetModal showModal={showModal} setShowModal={setShowModal} />
