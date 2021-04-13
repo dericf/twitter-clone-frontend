0041986ccd7d5e63dd66dfebdbd3846890016bdd
Deric F <dericgf@gmail.com>
2021-04-12 22:46:32 -0600

    Improve responsiveness - remove button text on mobile and leave only icon

--- components/Tweets/DeleteTweetModal.tsx
+++ components/Tweets/DeleteTweetModal.tsx
@@ -50,7 +50,7 @@ export const DeleteTweetModal: FunctionComponent<PropType> = (props) => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-4 w-4 mr-2"
+          className="h-4 w-4 sm:mr-2"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -62,7 +62,7 @@ export const DeleteTweetModal: FunctionComponent<PropType> = (props) => {
             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
           />
         </svg>
-        Delete
+        <span className="hidden sm:inline">Delete</span>
       </Button>
       {showModal && (
         <ConfirmModal
--- components/Tweets/EditTweetModal.tsx
+++ components/Tweets/EditTweetModal.tsx
@@ -151,7 +151,7 @@ export const EditTweetButton: FunctionComponent<PropType> = (props) => {
         className="flex justify-between items-center"
       >
         <svg
-          className="h-4 w-4 mr-2"
+          className="h-4 w-4 sm:mr-2"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -164,7 +164,7 @@ export const EditTweetButton: FunctionComponent<PropType> = (props) => {
             d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
           />
         </svg>
-        Edit
+        <span className="hidden sm:inline">Edit</span>
       </Button>
       {showModal && (
         <EditTweetModal
