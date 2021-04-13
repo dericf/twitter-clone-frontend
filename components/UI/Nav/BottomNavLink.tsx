85e9d2ba0b12a4e280a30d9f2c6929905d406e9d
Deric F <dericgf@gmail.com>
2021-04-12 22:47:08 -0600

    Implement proper bottom nav with all links

--- components/UI/Nav/BottomNavLink.tsx
+++ components/UI/Nav/BottomNavLink.tsx
@@ -1,23 +1,32 @@
 import Link from "next/link";
 import React, { FunctionComponent } from "react";
+import { useStore } from "../../../hooks/useStore";
+import { PageId } from "../../../schema/Navigation";
 
 type PropType = JSX.IntrinsicAttributes &
   React.ButtonHTMLAttributes<HTMLButtonElement> &
   React.AnchorHTMLAttributes<HTMLAnchorElement>;
-interface Props extends PropType {}
+interface Props extends PropType {
+  pageId: PageId;
+}
 
-export const BottomNavLink: FunctionComponent<PropType> = ({
+export const BottomNavLink: FunctionComponent<Props> = ({
   href,
   onClick,
   children,
+  pageId,
   ...props
 }) => {
   let colorClasses = "";
+  const { activePage } = useStore();
 
   return (
     <Link href={href} as={href}>
       <button
-        className={`py-4 px-2 font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-opacity-75 text-white bg-lightBlue-700 focus:ring-lightBlue-900 hover:bg-lightBlue-800  text-xs sm:text-sm md:text-lg flex-grow mx-auto rounded-none flex items-center justify-center `}
+        className={`py-4 px-2 shadow-md focus:outline-none focus:ring-2 focus:ring-opacity-75 text-white focus:ring-lightBlue-900 
+        hover:bg-lightBlue-800  text-xs sm:text-sm flex-grow mx-auto rounded-none flex items-center justify-center 
+        ${pageId === activePage ? "bg-lightBlue-800 " : "bg-lightBlue-600 "}
+        `}
         type="button"
         onClick={onClick}
         {...props}
--- components/UI/Nav/MobileBottomNav.tsx
+++ components/UI/Nav/MobileBottomNav.tsx
@@ -1,6 +1,8 @@
 import { Button } from "../Button";
 import Link from "next/link";
 import { BottomNavLink } from "./BottomNavLink";
+import { SidebarLinkI } from "../../../schema/Navigation";
+import { useStore } from "../../../hooks/useStore";
 
 // import { useRouter } from "next/router";
 // import { useEffect } from "react";
@@ -9,108 +11,171 @@ import { BottomNavLink } from "./BottomNavLink";
 // import { LogoutButton } from "./LogoutButton";
 // import { MainTitle } from "./MainAppTitle";
 
+const BOTTOM_NAV_LINKS: Array<SidebarLinkI> = [
+  {
+    href: "/",
+    id: "discover",
+    text: "",
+    svg: (
+      <svg
+        className="h-6 w-6 shadow-lg"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    ),
+  },
+  {
+    href: "/tweets",
+    id: "tweets",
+    text: "",
+    svg: (
+      <svg
+        className="h-6 w-6 shadow-lg"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+        />
+      </svg>
+    ),
+  },
+  {
+    href: "/likes",
+    id: "likes",
+    text: "",
+    svg: (
+      <svg
+        className="h-6 w-6 shadow-lg"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+        />
+      </svg>
+    ),
+  },
+  {
+    href: "/followers",
+    id: "followers",
+    text: "",
+    svg: (
+      <svg
+        className="h-6 w-6 shadow-lg"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+        />
+      </svg>
+    ),
+  },
+  {
+    href: "/following",
+    id: "following",
+    text: "",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 shadow-lg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+        />
+      </svg>
+    ),
+  },
+  {
+    href: "/comments",
+    id: "comments",
+    text: "",
+    svg: (
+      <svg
+        className="h-6 w-6 shadow-lg"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+        />
+      </svg>
+    ),
+  },
+  {
+    href: "/profile",
+    id: "profile",
+    text: "",
+    svg: (
+      <svg
+        className="h-6 w-6 shadow-lg"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    ),
+  },
+];
+
 export const MobileBottomNav = () => {
+  const { setActivePage } = useStore();
   return (
     <>
-      <BottomNavLink href="/">
-        <svg
-          className="h-6 w-6 shadow-lg"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+      {BOTTOM_NAV_LINKS.map((link) => (
+        <BottomNavLink
+          href={link.href}
+          key={link.href}
+          pageId={link.id}
+          onClick={() => setActivePage(link.id)}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </BottomNavLink>
-      <BottomNavLink href="/tweets">
-        <svg
-          className="h-6 w-6 shadow-lg"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-          />
-        </svg>
-      </BottomNavLink>
-
-      <BottomNavLink href="/followers">
-        <svg
-          className="h-6 w-6 shadow-lg"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-          />
-        </svg>
-      </BottomNavLink>
-
-      <BottomNavLink href="/likes">
-        <svg
-          className="h-6 w-6 shadow-lg"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-          />
-        </svg>
-      </BottomNavLink>
-      <BottomNavLink href="/comments">
-        <svg
-          className="h-6 w-6 shadow-lg"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
-          />
-        </svg>
-      </BottomNavLink>
-
-      <BottomNavLink href="/profile">
-        <svg
-          className="h-6 w-6 shadow-lg"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </BottomNavLink>
+          {link.text}
+          {link.svg}
+        </BottomNavLink>
+      ))}
     </>
   );
 };
