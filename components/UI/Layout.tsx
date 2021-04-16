5251cde868367e8de3891efbfdc8ade744034d1f
Deric F <dericgf@gmail.com>
2021-04-15 22:17:33 -0600

    Add _document.tsx and move all meta/link tags there instead of Layout

--- components/UI/Layout.tsx
+++ components/UI/Layout.tsx
@@ -89,28 +89,7 @@ export const Layout = ({
   return (
     <>
       <Head>
-        <title>{tabTitle} | Twitter Clone</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#0369a1"></meta>
+        <title>Twitter Clone | {tabTitle}</title>
       </Head>
       <main className=" w-screen h-screen flex flex-col overflow-hidden">
         <div className="flex flex-none px-2 py-2 w-full h-20 items-center justify-between shadow-xl  border-none bg-lightBlue-700 text-white">
---
+++ pages/_document.tsx
@@ -0,0 +1,49 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="description"
+            content="A Twitter-Like App used as an example project to teach new software developers about full-stack software development.
+					Author: Deric Fagnan"
+          ></meta>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#0369a1"></meta>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
