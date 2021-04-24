a245d3208def07cad69f113fee02a3c4a498cf7c
Deric F <dericgf@gmail.com>
2021-04-23 23:12:59 -0600

    Fix margin on main app title

--- components/UI/MainAppTitle.tsx
+++ components/UI/MainAppTitle.tsx
@@ -1,4 +1,7 @@
-
-export const MainTitle = () => {  
-  return <h1 className="text-center text-2xl  sm:text-3xl md:text-4xl">Twitter Clone</h1>;
+export const MainTitle = () => {
+  return (
+    <h1 className="mx-2 text-center text-2xl  sm:text-3xl md:text-4xl">
+      Twitter Clone
+    </h1>
+  );
 };
