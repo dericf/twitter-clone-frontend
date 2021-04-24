05f5afb68217ee5387f41cead2ba3a228baa75fe
Deric F <dericgf@gmail.com>
2021-04-23 22:58:59 -0600

    Add toast notifications when logging out

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -2,15 +2,19 @@ import { Layout } from "../components/UI/Layout";
 import { GetServerSideProps, NextPage } from "next";
 import { useEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
+import { useAlert } from "../hooks/useAlert";
 interface Props {}
 
 export default function Discover(props: Props) {
   const { logout } = useAuth();
+  const { sendAlert, sendError } = useAlert();
   useEffect(() => {
     (async () => {
       await logout();
+      sendAlert("You have been logged out.");
     })().catch((err) => {
       console.error(err);
+      sendError("There was an error logging you out. Please try again.");
     });
   }, []);
   return (
