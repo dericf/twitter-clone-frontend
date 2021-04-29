a50b4b37bed2c81bcac49872ade47fa0d82ce881
Deric F <dericgf@gmail.com>
2021-04-28 23:56:12 -0600

    Add messages link to navigation

--- components/UI/Nav/LeftSidebarNav.tsx
+++ components/UI/Nav/LeftSidebarNav.tsx
@@ -72,6 +72,27 @@ const SIDEBAR_LINKS: Array<SidebarLinkI> = [
     ),
   },
   {
+    href: "/comments",
+    id: "comments",
+    text: "Comments",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 shadow-lg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+        />
+      </svg>
+    ),
+  },
+  {
     href: "/followers",
     id: "followers",
     text: "Followers",
@@ -114,13 +135,13 @@ const SIDEBAR_LINKS: Array<SidebarLinkI> = [
     ),
   },
   {
-    href: "/comments",
-    id: "comments",
-    text: "Comments",
+    href: "/messages",
+    id: "messages",
+    text: "Messages",
     svg: (
       <svg
-        className="h-6 w-6 shadow-lg"
         xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 shadow-lg"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
@@ -129,7 +150,7 @@ const SIDEBAR_LINKS: Array<SidebarLinkI> = [
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth={2}
-          d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+          d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
         />
       </svg>
     ),
