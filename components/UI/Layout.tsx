b887c830d83ea9febe3d91e457f52285cdbea229
Deric F <dericgf@gmail.com>
2021-05-03 17:55:49 -0600

    Organize Layout imports + add proper Prop Types

--- components/UI/Layout.tsx
+++ components/UI/Layout.tsx
@@ -1,42 +1,52 @@
-// TODO: organize imports
+// React
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
+
+// NextJS
 import { useRouter } from "next/router";
-import React, { useEffect, useRef, useState } from "react";
+import Head from "next/head";
+
+// Hooks
 import { useAuth } from "../../hooks/useAuth";
+import { useAlert } from "../../hooks/useAlert";
+import { useStore } from "../../hooks/useStore";
+import { useEmitter } from "../../hooks/useEmitter";
+import ChatContextProvider from "../../hooks/useChat";
+
+// UI Components
 import { Button } from "./Button";
+import { MobileBottomNav } from "./Nav/MobileBottomNav";
+import { LeftSidebar } from "./Nav/LeftSidebarNav";
+import { LoadingOverlay } from "./LoadingOverlay";
+import { HamburgerIcon } from "./Icons/HamburgerIcon";
+
+// App Components
 import { LoginButton } from "./LoginButton";
 import { RegisterButton } from "./RegisterButton";
 import { LogoutButton } from "./LogoutButton";
 import { MainTitle } from "./MainAppTitle";
-import Link from "next/link";
-
-import { MobileBottomNav } from "./Nav/MobileBottomNav";
-import { LeftSidebar } from "./Nav/LeftSidebarNav";
 import { NewTweetButton } from "../Tweets/NewTweetButton";
-import { LoadingOverlay } from "./LoadingOverlay";
-import { useAlert } from "../../hooks/useAlert";
-import Head from "next/head";
-import { useStore } from "../../hooks/useStore";
-import { useEmitter } from "../../hooks/useEmitter";
-import { WSMessage } from "../../schema/WebSockets";
-
-import WSC from "../../websocket/client";
-import { Message } from "../../schema/Messages";
 import { ChatBar } from "../Chat/ChatBar";
-import ChatContextProvider from "../../hooks/useChat";
 
-import { HamburgerIcon } from "../ui/Icons/HamburgerIcon";
-
-// TODO: Add proper Prop Types
-export const Layout = ({
-  children,
-  isProtected = false,
-  noAuth = false,
-  silentAuth = true,
-  pageTitle = null,
-  tabTitle = pageTitle,
-  onAuthSuccess = () => {},
-  loading = false,
-}) => {
+interface Props {
+  isProtected: boolean;
+  noAuth: boolean;
+  silentAuth: boolean;
+  pageTitle: string;
+  tabTitle: string;
+  onAuthSuccess: () => void;
+  loading: boolean;
+}
+export const Layout: FunctionComponent<Props> = (props) => {
+  const {
+    children,
+    isProtected = false,
+    noAuth = false,
+    silentAuth = true,
+    pageTitle = null,
+    tabTitle = pageTitle,
+    onAuthSuccess = () => {},
+    loading = false,
+  } = props;
   const { user, isAuthenticated, loadAuthState } = useAuth();
   const { sendError, sendAlert } = useAlert();
   const [isLoading, setLoading] = useState(loading || !user);
