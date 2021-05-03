af8ce3896927e865c0d8cd1416e45426cdda5099
Deric F <dericgf@gmail.com>
2021-05-03 13:10:23 -0600

    Implement Messages/Chat Schema

---
+++ schema/Messages.ts
@@ -0,0 +1,51 @@
+// API
+import { APIResponse } from "./API";
+
+// Schema
+import { EmptyResponse } from "./General";
+
+export interface Message {
+  id: number;
+  userFromId: number;
+  userFromUsername: string;
+  userToId: number;
+  userToUsername: string;
+  content: string;
+  createdAt: Date;
+}
+
+export interface Conversation {
+  username: string;
+  userId: number;
+  messages: Message[];
+}
+
+export interface Conversations {
+  [userId: number]: Conversation;
+}
+
+export type ConversationsResponse = APIResponse<Conversations>;
+
+export interface MessageCreateRequestBody {
+  content: string;
+  userToId: number;
+}
+
+export type MessageCreateResponse = APIResponse<Message>;
+
+export interface MessageUpdateRequestBody {
+  messageId: number;
+  newContent: string;
+}
+
+export interface MessageUpdateResponse extends APIResponse<Message> {
+  //
+}
+
+export interface MessageDeleteRequestBody {
+  messageId: number;
+}
+
+export interface MessageDeleteResponse extends APIResponse<EmptyResponse> {
+  //
+}
