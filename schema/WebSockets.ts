bed13f9245b996ecfa31918f97258f1ce8a87f3b
Deric F <dericgf@gmail.com>
2021-05-07 21:26:25 -0600

    Clean up and minor changes to WebSocket schema

--- schema/WebSockets.ts
+++ schema/WebSockets.ts
@@ -1,19 +1,28 @@
-type WSAction = "conversations.get.all" | "messages.new" | "messages.delete";
+type WSAction =
+  | "auth.required"
+  | "chat.message.new"
+  | "chat.message.deleted"
+  | "chat.user.online"
+  | "chat.user.typing";
 
+interface WSMessageError {
+  message: string;
+  code: number;
+}
 export interface WSMessage<T> {
   action: WSAction;
-  status?: number;
+  status?: number; //? Shouldn't have status here since it isn't a response....
   body?: T | null;
+  error?: WSMessageError;
 }
 
 export interface WSResponse<T> {
   action: WSAction;
   status?: number;
   body?: T | null;
+  error?: WSMessageError;
 }
-
-export type WSMessageCode = "messages.new" | "messages.deleted";
 
 export type EmitterCallback = (message: WSMessage<any>) => void;
 
-export type WSSubscription = Map<WSMessageCode, EmitterCallback>;
+export type WSSubscription = Map<WSAction, EmitterCallback>;
