64f6c8bac7d2d9ff46be00c9051a9aa9a2ef7fa1
Deric F <dericgf@gmail.com>
2021-05-08 18:00:40 -0600

    Fix loading spinner size

--- components/UI/LoadingSpinner.tsx
+++ components/UI/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 export const LoadingSpinner = () => {
   return (
     <svg
-      className="h-10 w-10 mt-1 animate-spin text-white"
+      className="h-4 w-4  animate-spin text-white"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
